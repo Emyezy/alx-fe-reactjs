61f678335176e8c103885ae8e680c4a9bd5292b5
Emyezy <emmanueliwediba@yahoo.com>
2025-07-25 01:17:39 +0100

    Add styled components in alx-react-app-new

---
+++ alx-react-app-new/alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', padding: '15px', textAlign: 'center' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
