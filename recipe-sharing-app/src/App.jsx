f29bea81e49ca3ded4e2f242e614f711afda14c6
Iwediba Emmanuel <emmanueliwediba@yahoo.com>
2025-07-31 01:14:39 +0100

    Add recipe details, edit, and delete functionality with routing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import './App.css';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <hr />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<>
+          <h1>Recipe Sharing App</h1>
+          <AddRecipeForm />
+          <RecipeList />
+        </>} />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
