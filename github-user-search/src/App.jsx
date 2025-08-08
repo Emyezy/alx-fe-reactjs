f010108cee40e8adcea7895bfbe7518de0faefee
Emyezy <117361922+Emyezy@users.noreply.github.com>
2025-08-08 03:34:42 +0100

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <h1>GitHub User Search</h1>
-      <p>Start building your app here...</p>
+      <h1 style={{ textAlign: "center" }}>GitHub User Search</h1>
+      <Search />
     </div>
   );
 }
