50529b7acec1148aff5bf65a504970e3fb1b6016
Emyezy <117361922+Emyezy@users.noreply.github.com>
2025-08-31 23:33:43 +0100

    Update and rename FormikForm.jsx to formikForm.jsx

--- form-handling-react/src/components/formikForm.jsx
+++ form-handling-react/src/components/formikForm.jsx
@@ -5,7 +5,9 @@ import * as Yup from "yup";
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
   email: Yup.string().email("Invalid email").required("Email is required"),
-  password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
 });
 
 export default function FormikForm() {
