fb99e0c4b530b3f53658889962b14fbf3930c844
Emyezy <117361922+Emyezy@users.noreply.github.com>
2025-09-01 00:22:18 +0100

    Update BlogPost.jsx

--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 
-export default function BlogPost() {
+function BlogPost() {
   const { id } = useParams();
   return <h2>Blog Post ID: {id}</h2>;
 }
+
+export default BlogPost;
