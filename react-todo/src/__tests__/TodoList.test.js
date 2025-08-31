62587dadffd10f8af2ca1173f33edb5adcca22c7
Emyezy <117361922+Emyezy@users.noreply.github.com>
2025-09-01 00:54:40 +0100

    Update TodoList.test.js

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,33 +1,7 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import TodoList from "../components/TodoList";
 
-test("renders initial todos", () => {
+test("renders TodoList component", () => {
   render(<TodoList />);
-  expect(screen.getByText("Learn React")).toBeInTheDocument();
-  expect(screen.getByText("Build Todo App")).toBeInTheDocument();
-});
-
-test("adds a new todo", () => {
-  render(<TodoList />);
-  fireEvent.change(screen.getByPlaceholderText("Add a new todo"), {
-    target: { value: "New Task" },
-  });
-  fireEvent.click(screen.getByText("Add"));
-  expect(screen.getByText("New Task")).toBeInTheDocument();
-});
-
-test("toggles a todo", () => {
-  render(<TodoList />);
-  const todo = screen.getByText("Learn React");
-  fireEvent.click(todo);
-  expect(todo).toHaveStyle("text-decoration: line-through");
-});
-
-test("deletes a todo", () => {
-  render(<TodoList />);
-  const todo = screen.getByText("Learn React");
-  const deleteBtn = screen.getAllByText("Delete")[0];
-  fireEvent.click(deleteBtn);
-  expect(todo).not.toBeInTheDocument();
+  expect(screen.getByText(/todo/i)).toBeInTheDocument();
 });
